1ca49b25fe9f9e0adccc42e25c4e0ed0fce9665d
julianamendez11 <46967761+julianamendez11@users.noreply.github.com>
2019-05-23 11:07:31 -0500

    Create plot1.py

---
+++ plot1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt("datos1.dat")
+
+plt.figure()
+plt.plot(data[:,1], data[:,2])
+plt.xlabel("Coordenada X")
+plt.ylabel("Coordenada Y")
+
+plt.savefig("MendezJuliana_final_15.pdf")
